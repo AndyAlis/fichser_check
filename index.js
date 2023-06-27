8d5ca8cb9114b3f5c77538d6343cb02da81528dd
AndyAlis <lisligi@mail.ru>
2023-06-27 11:02:43 +0300

    change logo and add markers

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import {shops} from "./shops.js"
+
 const burger = document.querySelector(".header-burger");
 const mobLinks = document.querySelectorAll(".mobile-nav-link");
 
@@ -13,3 +15,70 @@ mobLinks.forEach(link => {
         document.getElementById("mobile-nav").classList.add("display-none");
     })
 })
+
+
+// *************************
+// yandex maps
+// https://yandex.ru/dev/jsapi30/doc/ru/?from=mapsapi
+// *************************
+ymaps3.ready.then(init);
+function init() {
+// Создание карты
+const map = new ymaps3.YMap(document.getElementById('map'), {
+    location: {
+    // Координаты центра карты
+    // Порядок по умолчанию: «долгота, широта»
+    // 55.75242679785425, 37.61755947247549 кремль
+    center: [37.61755947247549, 55.75242679785425 ],
+
+    // Уровень масштабирования
+    // Допустимые значения: от 0 (весь мир) до 21.
+    zoom: 7
+    }
+    }
+);
+// Добавляем слой для отображения схематической карты, при отключении - выключает карту
+    map.addChild(new ymaps3.YMapDefaultSchemeLayer());
+// слой для добавления маркеров
+    map.addChild(new ymaps3.YMapDefaultFeaturesLayer({zIndex: 1800}))
+// *************************
+// маркер 1
+// *************************
+
+    // const markerElement = document.createElement('div');
+    // markerElement.className = 'marker-class';
+    // // markerElement.innerText = "I'm marker!";
+
+    // const mark2 = new ymaps3.YMapMarker(
+    // {
+    //     // source: 'markerSource',
+    //     coordinates: [37.61755947247549, 55.75242679785425 ],
+    //     draggable: false,
+    //     mapFollowsOnDrag: true
+    // },
+    // markerElement
+    // );
+    // map.addChild(mark2);
+
+    
+    // *************************
+    // добавляем сразу несколько маркеров из массива
+    // *************************
+    
+    shops.forEach(el=>{
+        const markerElement = document.createElement('div');
+        markerElement.className = 'marker-class'; 
+
+        const mark = new ymaps3.YMapMarker(
+        {
+            // source: 'markerSource',
+            coordinates: el.coordinates,
+            draggable: false,
+            mapFollowsOnDrag: true
+        },
+        markerElement
+        );
+        map.addChild(mark);
+    });
+    
+}
