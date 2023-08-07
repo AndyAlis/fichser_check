34b60506a6c95f36a7248ce3c846ce7dae2acfa8
AndyAlis <lisligi@mail.ru>
2023-08-07 07:52:01 +0300

    andd smooth scroll and buttons transitions

--- index.js
+++ index.js
@@ -26,94 +26,98 @@ mobLinks.forEach((link) => {
 // yandex maps
 // https://yandex.ru/dev/jsapi30/doc/ru/?from=mapsapi
 // *************************
-ymaps3.ready.then(init);
-function init() {
-  // Создание карты
-  const map = new ymaps3.YMap(document.getElementById("map"), {
-    location: {
-      // Координаты центра карты
-      // Порядок по умолчанию: «долгота, широта»
-      center: [37.61755947247549, 55.75242679785425],
-      // Уровень масштабирования
-      // Допустимые значения: от 0 (весь мир) до 21.
-      zoom: 7,
-    },
-  });
-  // Добавляем слой для отображения схематической карты, при отключении - выключает карту
-  map.addChild(new ymaps3.YMapDefaultSchemeLayer());
-  // слой для добавления маркеров
-  map.addChild(new ymaps3.YMapDefaultFeaturesLayer({ zIndex: 1800 }));
-  // *************************
-  // маркер 1
-  // *************************
-
-  // const markerElement = document.createElement('div');
-  // markerElement.className = 'marker-class';
-  // // markerElement.innerText = "I'm marker!";
-
-  // const mark2 = new ymaps3.YMapMarker(
-  // {
-  //     // source: 'markerSource',
-  //     coordinates: [37.61755947247549, 55.75242679785425 ],
-  //     draggable: false,
-  //     mapFollowsOnDrag: true
-  // },
-  // markerElement
-  // );
-  // map.addChild(mark2);
-
-  // *************************
-  // добавляем сразу несколько маркеров из массива
-  // *************************
-
-  shops.forEach((el) => {
-    let isShown = false;
-
-    const markerElement = document.createElement("div");
-    markerElement.className = "marker-class";
-
-    const dialogElement = document.createElement("dialog");
-    dialogElement.className = "dialogElement";
-
-    const dialogHeader = document.createElement("h3");
-    dialogHeader.textContent = `${el.name}`;
-
-    const dialogAddress = document.createElement("p");
-    dialogAddress.textContent = `${el.address || ""}`;
-
-    const dialogPhone = document.createElement("p");
-    const dialogPhoneLink = document.createElement("a");
-    dialogPhoneLink.textContent = `${el.phone || ""}`;
-    dialogPhoneLink.href = `tel:${el.phoneLink || ""}`;
-    dialogPhone.appendChild(dialogPhoneLink);
-
-    dialogElement.appendChild(dialogHeader);
-    dialogElement.appendChild(dialogAddress);
-    dialogElement.appendChild(dialogPhone);
-    markerElement.appendChild(dialogElement);
-
-    markerElement.addEventListener("click", () => {
-      // window.alert(el.name);
-      if (!isShown) {
-        dialogElement.showModal();
-        isShown = !isShown;
-      } else {
-        dialogElement.close();
-        isShown = !isShown;
-      }
-    });
-
-    const mark = new ymaps3.YMapMarker(
-      {
-        // source: 'markerSource',
-        coordinates: el.coordinates,
-        draggable: false,
-        mapFollowsOnDrag: true,
+try {
+  ymaps3.ready.then(init);
+  function init() {
+    // Создание карты
+    const map = new ymaps3.YMap(document.getElementById("map"), {
+      location: {
+        // Координаты центра карты
+        // Порядок по умолчанию: «долгота, широта»
+        center: [37.61755947247549, 55.75242679785425],
+        // Уровень масштабирования
+        // Допустимые значения: от 0 (весь мир) до 21.
+        zoom: 7,
       },
-      markerElement
-    );
-    map.addChild(mark);
-  });
+    });
+    // Добавляем слой для отображения схематической карты, при отключении - выключает карту
+    map.addChild(new ymaps3.YMapDefaultSchemeLayer());
+    // слой для добавления маркеров
+    map.addChild(new ymaps3.YMapDefaultFeaturesLayer({ zIndex: 1800 }));
+    // *************************
+    // маркер 1
+    // *************************
+
+    // const markerElement = document.createElement('div');
+    // markerElement.className = 'marker-class';
+    // // markerElement.innerText = "I'm marker!";
+
+    // const mark2 = new ymaps3.YMapMarker(
+    // {
+    //     // source: 'markerSource',
+    //     coordinates: [37.61755947247549, 55.75242679785425 ],
+    //     draggable: false,
+    //     mapFollowsOnDrag: true
+    // },
+    // markerElement
+    // );
+    // map.addChild(mark2);
+
+    // *************************
+    // добавляем сразу несколько маркеров из массива
+    // *************************
+
+    shops.forEach((el) => {
+      let isShown = false;
+
+      const markerElement = document.createElement("div");
+      markerElement.className = "marker-class";
+
+      const dialogElement = document.createElement("dialog");
+      dialogElement.className = "dialogElement";
+
+      const dialogHeader = document.createElement("h3");
+      dialogHeader.textContent = `${el.name}`;
+
+      const dialogAddress = document.createElement("p");
+      dialogAddress.textContent = `${el.address || ""}`;
+
+      const dialogPhone = document.createElement("p");
+      const dialogPhoneLink = document.createElement("a");
+      dialogPhoneLink.textContent = `${el.phone || ""}`;
+      dialogPhoneLink.href = `tel:${el.phoneLink || ""}`;
+      dialogPhone.appendChild(dialogPhoneLink);
+
+      dialogElement.appendChild(dialogHeader);
+      dialogElement.appendChild(dialogAddress);
+      dialogElement.appendChild(dialogPhone);
+      markerElement.appendChild(dialogElement);
+
+      markerElement.addEventListener("click", () => {
+        // window.alert(el.name);
+        if (!isShown) {
+          dialogElement.showModal();
+          isShown = !isShown;
+        } else {
+          dialogElement.close();
+          isShown = !isShown;
+        }
+      });
+
+      const mark = new ymaps3.YMapMarker(
+        {
+          // source: 'markerSource',
+          coordinates: el.coordinates,
+          draggable: false,
+          mapFollowsOnDrag: true,
+        },
+        markerElement
+      );
+      map.addChild(mark);
+    });
+  }
+} catch (err) {
+  console.log(err);
 }
 
 form.addEventListener("submit", (e) => {
@@ -122,6 +126,7 @@ form.addEventListener("submit", (e) => {
 });
 
 btn_address_list.addEventListener("click", () => {
+  // window.alert("Пока не работает!");
   map.classList.add("display-none");
   address_list.classList.remove("display-none");
   btn_address_list.classList.add("display-none");
@@ -133,4 +138,36 @@ btn_address_map.addEventListener("click", () => {
   address_list.classList.add("display-none");
   btn_address_map.classList.add("display-none");
   btn_address_list.classList.remove("display-none");
+});
+
+//creating adress list
+shops.forEach((el) => {
+  const shop = document.createElement("div");
+  shop.className = "shop";
+
+  const shopHeader = document.createElement("h3");
+  shopHeader.textContent = `${el.name}`;
+
+  const shopAddress = document.createElement("p");
+  shopAddress.textContent = `${el.address || ""}`;
+
+  const shopPhone = document.createElement("p");
+  const shopPhoneLink = document.createElement("a");
+  shopPhoneLink.textContent = `${el.phone || ""}`;
+  shopPhoneLink.href = `tel:${el.phoneLink || ""}`;
+  shopPhone.appendChild(shopPhoneLink);
+
+  const shopSite = document.createElement("p");
+  const shopSiteLink = document.createElement("a");
+  shopSiteLink.textContent = `${el.site || ""}`;
+  shopSiteLink.href = `${el.site || ""}`;
+  shopSiteLink.target = `_blank`;
+  shopSite.appendChild(shopSiteLink);
+
+  shop.appendChild(shopHeader);
+  shop.appendChild(shopAddress);
+  shop.appendChild(shopPhone);
+  shop.appendChild(shopSite);
+
+  address_list.appendChild(shop);
 });
